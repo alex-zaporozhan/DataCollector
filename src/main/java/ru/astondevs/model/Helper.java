7a7a65131910303cde738f5c8f6b8458f266932f
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-12-03 19:36:50 +0300

    class App: runnableMainThread, handleNewFile, readFileWithHelper

--- src/main/java/ru/astondevs/model/Helper.java
+++ src/main/java/ru/astondevs/model/Helper.java
@@ -1,4 +1,19 @@
 package ru.astondevs.model;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public interface Helper {
+    String CSV_DELIMITER = ";";
+    String getExtention(); //должен возвращять расширение файлов
+    int readRecords(InputStream inputStream); //-1 произошла ошибка; 0 нет записи; или число записей которые прочитали
+    boolean writeRecords(OutputStream outputStream); //возвращает false если произошла ошибка, нужно заканчивать приложение
+    void setSorting(String field);//настраивает сортировку
 }
+
+//запись создается через builder через addField
+// new Builder addField
+//проверяю если isValued
+//если true + в список
+//если нет тогда "извините не удалось"
